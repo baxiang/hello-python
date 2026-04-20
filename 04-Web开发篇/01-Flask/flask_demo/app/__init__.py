0899d967a42bed62f280cf642f0e9b901abc2572
baxiang <baxiang@lixiang.com>
2026-04-21 01:34:37 +0800

    fix: 修复目录迁移后Flask CMS API文件完整性

--- 04-Web开发篇/01-Flask/flask_demo/app/__init__.py
+++ 04-Web开发篇/01-Flask/flask_demo/app/__init__.py
@@ -1,3 +1,24 @@
-"""Flask 模块"""
+"""Flask CMS API应用"""
+
+from flask import Flask
+from app.extensions import db, jwt, migrate
+from app.api import register_blueprints
+
+
+def create_app(config_name: str = "development") -> Flask:
+    """Flask应用工厂"""
+    app = Flask(__name__, instance_relative_config=True)
+    
+    from app.config import config
+    app.config.from_object(config[config_name])
+    
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+    
+    register_blueprints(app)
+    
+    return app
+
 
 __version__ = "0.1.0"
--- 04-Web开发篇/01-Flask/flask_demo/app/models/__init__.py
+++ 04-Web开发篇/01-Flask/flask_demo/app/models/__init__.py
@@ -1,1 +1,6 @@
-"""模型模块"""
+"""数据模型"""
+
+from app.models.user import User
+from app.models.article import Article
+
+__all__ = ["User", "Article"]
